d228af545654bceac91968b4b987a9e3c9d3e3df
timkoffy <nikitakirilltimur@gmail.com>
2025-07-12 21:09:07 +0300

    сломанный текстбокс

--- WeatherAppAvalonia/ViewModels/MainViewModel.cs
+++ WeatherAppAvalonia/ViewModels/MainViewModel.cs
@@ -4,7 +4,5 @@ namespace WeatherAppAvalonia.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    public string ColorBoxId => "#0F151C";
-    public string ColorBackGroundId => "#181F28";
-    public string ColorSubTextId => "#475971";
+    
 }
