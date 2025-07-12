5259800ea0aa87a3c5ea63cc3b20e89c1da646c9
timkoffy <nikitakirilltimur@gmail.com>
2025-07-12 15:58:34 +0300

    vertex

--- WeatherAppAvalonia/ViewModels/MainViewModel.cs
+++ WeatherAppAvalonia/ViewModels/MainViewModel.cs
@@ -6,5 +6,5 @@ public partial class MainViewModel : ViewModelBase
 {
     public string ColorBoxId => "#0F151C";
     public string ColorBackGroundId => "#181F28";
-    
+    public string ColorSubTextId => "#475971";
 }
