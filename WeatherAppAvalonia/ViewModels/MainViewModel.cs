d2a1bc69bb6e55d29d4102c053ed01ff704a44b4
timkoffy <nikitakirilltimur@gmail.com>
2025-07-12 12:22:10 +0300

    first commit

---
+++ WeatherAppAvalonia/ViewModels/MainViewModel.cs
@@ -0,0 +1,8 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace WeatherAppAvalonia.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    public string Greetings => "Welcome";
+}
---
+++ WeatherAppAvalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace WeatherAppAvalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
